6a8d4587306df3ee433c77c24139d3793e8bb194
satyam-kalihari <satyamkalihari17@gmail.com>
2025-01-15 23:27:40 +0530

    Remove Duplicates from Sorted Array

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static int removeDuplicates(int[] nums){
+        Arrays.sort(nums);
+        int index = 1;
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] != nums[i-1]){
+                nums[index] = nums[i];
+                index++ ;
+            }
+        }
+        return index;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] nums;
+        nums = new int[n];
+
+        for (int i = 0; i < n; i++){
+            nums[i] = scn.nextInt();
+        }
+
+        int k = removeDuplicates(nums);
+        System.out.println("Length of non-duplicate Array: " + k);
+
+        System.out.print("Unique elements: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
